93ba87c708ce9d842ae86520ad02be8b3eb3f4aa
Sang Penyalang <104961991+SangPenyalang@users.noreply.github.com>
2025-03-18 11:57:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://Raziq:Raziq@cluster0.px1iv.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
